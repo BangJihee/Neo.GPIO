7645a1ef3e7f0efffce38e34a17f8b09d036fc0d
Bangjihee <2222wlgml@gmail.com>
2019-02-14 13:59:26 -0800

    My project_LED turn on / off_re

--- Temp.py
+++ Temp.py
@@ -13,30 +13,31 @@ pinNum = [S0, S1, S2, S3]
 
 num = [0,0,0,0]
 
+while 1:
 # Blink example
-for i in range(4):
-    neo.pinMode(pinNum[i], neo.OUTPUT)
+    for i in range(4):
+        neo.pinMode(pinNum[i], neo.OUTPUT)
 
-neo.digitalWrite(pinNum[0], 0)
-sleep(0.5)
-neo.digitalWrite(pinNum[1], 0)
-sleep(0.5)
-neo.digitalWrite(pinNum[2], 1)
-sleep(0.5)
-neo.digitalWrite(pinNum[3], 1)
-sleep(0.5)
+    neo.digitalWrite(pinNum[0], 0)
+    sleep(0.5)
+    neo.digitalWrite(pinNum[1], 0)
+    sleep(0.5)
+    neo.digitalWrite(pinNum[2], 1)
+    sleep(0.5)
+    neo.digitalWrite(pinNum[3], 1)
+    sleep(0.5)
 
-raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
-v = raw * scale
-t = (v - 500) / 10 - 6
-sleep(1)
-print(t)
+    raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+    scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
+    v = raw * scale
+    t = (v - 500) / 10 - 6
+    sleep(1)
+    print(t)
 
-'''
-raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
-print (raw * scale)
-'''
+    '''
+    raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+    scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
+    print (raw * scale)
+    '''
 
 
