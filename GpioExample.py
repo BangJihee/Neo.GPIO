4a54b612e5cb84347e9e2f142081ed24696110dd
Bangjihee <2222wlgml@gmail.com>
2019-02-14 10:20:58 -0800

    My project_LED turn on / off_re

--- GpioExample.py
+++ GpioExample.py
@@ -30,11 +30,5 @@ raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
 scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
 print (raw * scale)
 '''
-from time import sleep
-while True:
-    raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-    scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
-    v = raw * scale
-    t = (v - 500)/10 - 6
-    sleep(0.5)
-    print(t)
+
+
