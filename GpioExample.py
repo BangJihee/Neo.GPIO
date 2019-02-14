a5a91459b865f5daffb60c8a7e9f6f6cb05470ea
Bangjihee <2222wlgml@gmail.com>
2019-02-13 22:31:42 -0800

    My project_LED turn on / off_re

--- GpioExample.py
+++ GpioExample.py
@@ -1,4 +1,4 @@
-from neo import Gpio  # import Gpio library
+'''from neo import Gpio  # import Gpio library
 from time import sleep  # import sleep to wait for blinks
 
 
@@ -29,3 +29,12 @@ sleep(0.5)
 raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
 scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
 print (raw * scale)
+'''
+from time import sleep
+while True:
+    raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+    scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
+    v = raw * scale
+    t = (v - 500)/10 - 6
+    sleep(0.5)
+    print(t)
