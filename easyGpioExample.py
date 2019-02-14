d67f2cf84f09f8061a81c9d97b8433840ef443e8
Bangjihee <2222wlgml@gmail.com>
2019-02-14 10:34:24 -0800

    My project_LED turn on / off_re

--- easyGpioExample.py
+++ easyGpioExample.py
@@ -13,7 +13,7 @@ readpin.pinIN()  # Make pin in
 
 while True:
     pin.on()  # Turn pin on
-    sleep(1)  # wait one second
+    sleep(0.1)  # wait one second
     pin.off()  # Turn pin off
     print("pin 3 state %d" % readpin.get())  # Get current pin state
     sleep(1)
